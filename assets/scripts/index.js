1cde1974ab0ff5260201c5abc76ebb81cc9b880f
Rashmi Singh <rasmi2114@gmail.com>
2026-04-25 19:51:46 +0200

    Celsius to Fahrenheit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,19 @@
-const name = 'alok';
-namee = 'alok kumar';
-let sum =name;
 
+// Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+    let fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit;
+}
 
+// Fahrenheit to Celsius
+function fahrenheitToCelsius(fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5/9;
+    return celsius;
+}
 
-console.log(name);
+// Example use
+let tempC = 25;
+let tempF = 77;
+
+console.log(tempC + "°C = " + celsiusToFahrenheit(tempC) + "°F");
+console.log(tempF + "°F = " + fahrenheitToCelsius(tempF) + "°C");
