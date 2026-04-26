b145ca016a982b24b02015ddc913393fd81c01c4
Rashmi Singh <rasmi2114@gmail.com>
2026-04-26 18:55:02 +0200

    task changes

--- assets/scripts/task.js
+++ assets/scripts/task.js
@@ -81,3 +81,22 @@ namee = 'rashmi singh';
 let sum =name;
 
 console.log(name);
+
+// Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+    let fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit;
+}
+
+// Fahrenheit to Celsius
+function fahrenheitToCelsius(fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5/9;
+    return celsius;
+}
+
+// Example use
+let tempC = 25;
+let tempF = 77;
+
+console.log(tempC + "°C = " + celsiusToFahrenheit(tempC) + "°F");
+console.log(tempF + "°F = " + fahrenheitToCelsius(tempF) + "°C");
