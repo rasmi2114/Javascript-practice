ecd3bb58a1f142321eb19bf36b10d95e6eb438cd
Rashmi Singh <rasmi2114@gmail.com>
2026-05-06 16:36:47 +0200

    if else exapme

--- assets/scripts/task.js
+++ assets/scripts/task.js
@@ -114,3 +114,11 @@ function solve() {
     }
     userCategory = 'senior';
 }
+
+let age = 20;
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are under 18.");
+}
