1817824cd891728052138068f4b1219e3d60ab25
Rashmi Singh <rasmi2114@gmail.com>
2026-05-07 16:27:57 +0200

    statement

--- assets/scripts/task.js
+++ assets/scripts/task.js
@@ -162,3 +162,14 @@ if (hour < 18) {
 } else {
     console.log("Good Evening");
 }
+
+//Simple if else if statement
+let score = 85;
+
+if (score >= 90) {
+    console.log("Grade A");
+} else if (score >= 70) {
+    console.log("Grade B");
+} else {
+    console.log("Grade C");
+}
