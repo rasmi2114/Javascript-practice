d077cda7a590566ff4acce7d6b6690e9f734b688
Rashmi Singh <rasmi2114@gmail.com>
2026-05-20 18:22:50 +0200

    detail page

---
+++ assets/scripts/detail.js
@@ -0,0 +1,57 @@
+/* new code while using If else statement to check for operator and perform calculation accordingly */
+
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+// Gets input from input field
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+// Generates and writes calculation log
+function calculateResult(calculationType){
+  const enteredNumbar = getUserNumberInput();
+  const initialResult = currentResult;
+  let mathOperator;
+  if(calculationType === 'ADD') {
+    currentResult += enteredNumbar;
+    mathOperator = '+';
+  } else if(calculationType === 'SUBTRACT'){
+    currentResult -= enteredNumbar;
+    mathOperator = '-';
+  } else if(calculationType === 'MULTIPLY'){
+    currentResult *= enteredNumbar;
+    mathOperator = '*';
+  } else if(calculationType === 'DIVIDE'){
+    currentResult /= enteredNumbar;
+    mathOperator = '/';
+  }
+
+  createAndWriteOutput(mathOperator, initialResult, enteredNumbar);
+  writeToLog(calculationType, initialResult, enteredNumbar, currentResult);
+  
+}
+
+//add to divide all functions
+function add() {
+  calculateResult('ADD');
+}
+
+function subtract(){
+  calculateResult('SUBTRACT');
+}
+
+function multiply(){
+  calculateResult('MULTIPLY');
+}
+
+function divide(){
+  calculateResult('DIVIDE');
+}
+
+//all events
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
