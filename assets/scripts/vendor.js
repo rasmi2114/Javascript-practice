82763585c7be1db6d35e0d3da319373840db8009
Rashmi Singh <rasmi2114@gmail.com>
2026-05-30 09:22:37 +0200

    changess

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -8,20 +8,17 @@ const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
 
 // Output result to the user
-
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
 }
 
 //logic for calculation
-
 function writeToLog(operation, prevResult, number, newResult) {
   console.log(`${prevResult} ${operation} ${number} = ${newResult}`);
 }
 
 //calculator logic
-
 function createAndWriteOutput(mathOperator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${mathOperator} ${calcNumber}`;
   outputResult(currentResult, calcDescription); 
