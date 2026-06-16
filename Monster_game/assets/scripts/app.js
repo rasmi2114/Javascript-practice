52540fabe103cb6f77005f29b2c92c0be2b7ecd8
Rashmi Singh <rasmi2114@gmail.com>
2026-06-16 16:06:39 +0200

    battleLog

--- Monster_game/assets/scripts/app.js
+++ Monster_game/assets/scripts/app.js
@@ -221,6 +221,16 @@ function healPlayerHandler() {
 
 
 function printLogHandler() {
+    for (let i = 0; i < 3; i++){
+        console.log('------------------');
+    }
+    // for (let i = 10; i > 0;){
+    //     i--;
+    //     console.log(i);
+    // }
+    for(let i =0; i < battleLog.length; i++) {
+        console.log(battleLog[i]);
+    }
     console.log(battleLog);
 }   
 
