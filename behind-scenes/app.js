5743982ad95950f0e31eb8e3ee7b5579f606b7dc
Rashmi Singh <rasmi2114@gmail.com>
2026-06-26 12:11:25 +0200

    let,var const

---
+++ behind-scenes/app.js
@@ -0,0 +1,23 @@
+let name = 'Max';
+
+if(name === 'Max'){
+    let hobbies = ['sports', 'cooking'];
+    console.log(hobbies);
+}
+
+function greet() {
+  let age = 30;
+  let name = 'Manuel';
+  console.log(name, age);
+}
+
+//console.log(name, hobbies);
+
+greet();
+// 'use strict';
+
+// const userName = 'Max';
+// var undefined = 5;
+
+// console.log(userName);
+
