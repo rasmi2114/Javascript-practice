4237832643dd96ee4dfa41dbd2adda5dc51dc838
Rashmi Singh <rasmi2114@gmail.com>
2026-06-29 14:00:18 +0200

    error solve

--- behind-scenes/app.js
+++ behind-scenes/app.js
@@ -1,19 +1,19 @@
-let name = 'Max';
+// let name = 'Max';
 
-if(name === 'Max'){
-    let hobbies = ['sports', 'cooking'];
-    console.log(hobbies);
-}
+// if(name === 'Max'){
+//     let hobbies = ['sports', 'cooking'];
+//     console.log(hobbies);
+// }
 
-function greet() {
-  let age = 30;
-  let name = 'Manuel';
-  console.log(name, age);
-}
+// function greet() {
+//   let age = 30;
+//   let name = 'Manuel';
+//   console.log(name, age);
+// }
 
-console.log(name, hobbies);
+// console.log(name, hobbies);
 
-greet();
+// greet();
 // 'use strict';
 
 // const name = 'Max';
