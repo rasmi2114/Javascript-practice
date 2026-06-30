7628ad68b95b827b63ae15f7fd6db10451834de2
Rashmi Singh <rasmi2114@gmail.com>
2026-06-30 14:31:46 +0200

    messageInput

--- behind-scenes/app.js
+++ behind-scenes/app.js
@@ -1,24 +1,24 @@
-// let name = 'Max';
+let name = 'Max';
 
-// if(name === 'Max'){
-//     let hobbies = ['sports', 'cooking'];
-//     console.log(hobbies);
-// }
+if(name === 'Max'){
+    let hobbies = ['sports', 'cooking'];
+    console.log(hobbies);
+}
 
-// function greet() {
-//   let age = 30;
-//   let name = 'Manuel';
-//   console.log(name, age);
-// }
+function greet() {
+  let age = 30;
+  let name = 'Manuel';
+  console.log(name, age);
+}
 
-// console.log(name, hobbies);
+console.log(name, hobbies);
 
-// greet();
-// 'use strict';
+greet();
+'use strict';
 
-// const name = 'Max';
-// alert(name);
-// var undefined = 5;
+const name = 'Max';
+alert(name);
+var undefined = 5;
 
 // console.log(userName);
 const addListenerBtn = document.getElementById('add-listener-btn');
