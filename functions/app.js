f8a009ea1f2378fead744a3ba26ac28840efe332
Rashmi Singh <rasmi2114@gmail.com>
2026-07-01 13:09:08 +0200

    DEFAULT_USER_CHOICE

--- functions/app.js
+++ functions/app.js
@@ -1,5 +1,47 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const ROCK = 'ROCK';
+const  PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+let gameIsRunning = false;
+
+const getPlayerChoice = function() {
+  const selection = prompt(' ${ROCK}, ${PAPER} or ${SCISSORS}?', '').toUpperCase();
+  if (selection !== ROCK &&
+      selection !== PAPER &&
+      selection !== SCISSORS
+  ) {
+  alert ('Invalide choice! we chose ${DEFAULT_USER_CHOICE} for you!');
+  return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener('click', function(){
+  if (gameIsRunning){
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is started now...');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+});
+
+
+// *******create a methods ********
+
+// const person ={
+//   name: 'max',
+//   greet: function greet() {
+//     console.log ('hello there');
+//   }
+// }
+
+// person.greet();
+
+//************* 
+
 // const ROCK = 'ROCK';
 // const PAPER = 'PAPER';
 // const SCISSORS = 'SCISSORS';
