4585ab9a4e917b97f3f9b7b371c4dcb0d59cbf65
Rashmi Singh <rasmi2114@gmail.com>
2026-07-09 14:35:13 +0200

    switch

--- Intro/assets/scripts/info.js
+++ Intro/assets/scripts/info.js
@@ -1,12 +1,26 @@
 
-console.log("Hello");
+// console.log("Hello");
 
-let a = 10;
-let b = 20;
+// let a = 10;
+// let b = 20;
 
-console.log(a + b);
+// console.log(a + b);
 
-var namne =rashmi;
-console.log(name);
+// var namne =rashmi;
+// console.log(name);
 
 
+ switch (expression) {
+  case value1:
+    //   Code to be executed if expression === value1
+    break;
+
+  case value2:
+    //   Code to be executed if expression === value2
+    break;
+
+  //  More cases can be added as needed
+
+  default:
+  //  Code to be executed if none of the cases match
+}
