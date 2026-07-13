b5310acfda83a3ee000d96a8035b43bcd9a8e196
Rashmi Singh <rasmi2114@gmail.com>
2026-07-13 11:08:40 +0200

    isPalindrome

--- functions/fat_arrow.js
+++ functions/fat_arrow.js
@@ -1,6 +1,36 @@
-const sum = (a, b) => {
-    let result = `The sum of ${a} and ${b} is ${a + b}`;
-    console.log(result);
-};
+// const sum = (a, b) => {
+//     let result = `The sum of ${a} and ${b} is ${a + b}`;
+//     console.log(result);
+// };
 
-sum(6, 10);
+// sum(6, 10);
+
+// const sum = (a, b) => console.log( `The sum of ${a} and ${b} is ${a + b}.`);
+// sum(6, 10);
+
+// //Resversed a string
+// const isreverse = (str) => {
+//     let reverse = "";
+//     for (let char = str.length - 1; char >= 0; char--) {
+//         reverse = reverse + str[char];
+//     }
+//    return reverse;
+// };
+
+// console.log(isreverse("Rashmi Alok"));
+
+//Palindrome
+const isPalindrome = (str) => {
+    let reverse = "";
+    for (let char = str.length - 1; char >= 0; char--) {
+        reverse = reverse + str[char];
+    }
+    if (str === reverse) {
+        return console.log(`${str} is a palindrome`);
+    }else {
+        return console.log(`${str} is not a palindrome`);   
+    }
+
+    };
+
+   console.log(isPalindrome("level"));
