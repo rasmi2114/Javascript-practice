62f5e4af4aa41a105af9f396742ccd20af5b21e3
Rashmi Singh <rasmi2114@gmail.com>
2026-07-13 13:33:09 +0200

    forEach

--- functions/array.js
+++ functions/array.js
@@ -1,5 +1,6 @@
  //Array Traversal / Iterating Over Arrays
-//? 1: for of loop , also known as iterable
+//1: for of loop , also known as iterable : to display the items in the array
+//2 : for in loop : to display the index no of items in the array
 
 let fruits =["apple", "banana", "mango", "grapes", "kiwi"];
 // for (let items of fruits) {
@@ -7,10 +8,17 @@ let fruits =["apple", "banana", "mango", "grapes", "kiwi"];
 // }
 
 //to display the index no of items in the array
-for (let item in fruits) {
-    console.log(item);
-}
+// for (let item in fruits) {
+//     console.log(item);
+// }
+
+// not usefule in this case because it will display the index no of items in the array not the items itself
 
 // for(let item = 0; item < fruits.length; item++) {
 //     console.log(fruits[item]);
 // }
+
+//forEach() method : to display the items in the array
+fruits.forEach((curElem, index, array) => {
+    console.log(`${curElem} ${index}`);
+});
