fce80973c1722a0f274772c35f091c582afb87ed
Rashmi Singh <rasmi2114@gmail.com>
2026-07-14 10:59:45 +0200

    compare

--- functions/array.js
+++ functions/array.js
@@ -91,9 +91,38 @@
 
 
 //filter() method : to find all the items in the array that satisfies the condition
-    const number =[1, 2, 3, 4, 5, 6, 4, 7, 8, 9, 4, 10];
-    const result3 = number.filter((curElem) => {
-        return curElem > 5;
-    });
-    console.log(result3); // [6, 7, 8, 9, 10]
-        
+    // const number =[1, 2, 3, 4, 5, 6, 4, 7, 8, 9, 4, 10];
+    // const result3 = number.filter((curElem) => {
+    //     return curElem > 5;
+    // });
+    // console.log(result3); // [6, 7, 8, 9, 10]
+    
+
+//UseCase: In E-commerce website when we want to Remove or delete any product from addToCart page.
+//! Ex. le'ts say user wants to delete value 6.
+        // let value = 6;
+        // const numbers = [1, 2, 3, 4, 6, 5, 6, 7, 8, 9];  
+        // let updatecart = numbers.filter((curElem) => {
+        //     return curElem !== value;
+        // });
+        // console.log(updatecart); // [1, 2, 3, 4, 5, 7, 8, 9]
+
+// sort() method : to sort the items in the array
+    // let fruits =["apple", "banana", "mango", "grapes", "kiwi", "orange"];
+    // fruits.sort();
+    // console.log(fruits); // [ 'apple', 'banana', 'grapes', 'kiwi', 'mango', 'orange' ]   
+
+
+// compare() method : to sort the items in the array in ascending or descending order
+        // number = [1, 2, 3, 2, 4, 5, 6, 7, 8, 6, 9, 10];
+        // //ascending order or default order
+        // number.sort((a, b) => {
+        //     if (a < b) {
+        //         return 1;   //return -1
+        //     }
+        //     if (a > b) {
+        //         return -1;  // return 1
+        //     }
+        //     return 0;
+        // });
+        // console.log(number); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
