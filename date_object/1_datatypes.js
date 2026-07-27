f1630074de77f6a881b512709a2e01fe55d2b81f
Rashmi Singh <rasmi2114@gmail.com>
2026-07-27 11:45:06 +0200

    .

--- date_object/1_datatypes.js
+++ date_object/1_datatypes.js
@@ -2,6 +2,27 @@
 //* Data Types Section
 //* ============================
 
+// 9 ways to create a new Date Objects
+
+// 1. new Date()
+
+// 2. new Date(date string)
+
+// 3. new Date(year, month)
+
+// 4. new Date(year, month, day)
+
+// 5. new Date(year, month, day, hours)
+
+// 6. new Date(year, month, day, hours, minutes)
+
+// 7. new Date(year, month, day, hours, minutes, seconds)
+
+// 8. new Date(year, month, day, hours, minutes, seconds, ms)
+
+// 9. new Date(milliseconds)
+
+
 //* Data types define the type of values that a variable can hold.
 
 //? Types of Primitive Data types
