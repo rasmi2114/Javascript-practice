777f8885b700fb98477ccf09f63ba4fb8562c355
Rashmi Singh <rasmi2114@gmail.com>
2026-08-02 12:18:33 +0200

    multiplier

--- java_script_work/closure.js
+++ java_script_work/closure.js
@@ -21,7 +21,8 @@
 
 // We just need to know that an inner function has always access to the variables of the outer (parent) function. That's it.
 
-// Why is it like that? It's because the variable object of the parent function stays in memory even after the function returns, and our inner function has access to this variable object through the scope chain.
+// Why is it like that? It's because the variable object of the parent function stays in memory even after the function returns,
+//  and our inner function has access to this variable object through the scope chain.
 
 // It might be beneficial to clarify that the call stack is simply the execution order and it is not the same thing as the computer's memory, which is where the variables are held.  So even though the function was removed from the call stack, its variables are still maintained in memory until they are no longer needed and garbage collection comes and removes them.
 
