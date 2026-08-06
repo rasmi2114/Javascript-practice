39bd6afb72ddf9f82b4d3c5cec8c00c02e20ce7f
Rashmi Singh <rasmi2114@gmail.com>
2026-08-06 12:49:00 +0200

    Callback hell

--- advance_jscript/callback-hell.js
+++ advance_jscript/callback-hell.js
@@ -4,9 +4,34 @@
 
 //? Callback hell, also known as the Pyramid of Doom, refers to a situation in asynchronous JavaScript programming where multiple nested callbacks are used to handle asynchronous operations. This often results in code that is difficult to read, understand, and maintain due to its deeply nested structure.
 
-// write the code with me here only ok
-    const getStudentData = () => {
-        
-    }
+   function getUser(callback) {
+    setTimeout(() => {
+        console.log("User fetched");
+        callback();
+    }, 1000);
+}
 
-    getStudentData();
+function getOrders(callback) {
+    setTimeout(() => {
+        console.log("Orders fetched");
+        callback();
+    }, 1000);
+}
+
+function getOrderDetails(callback) {
+    setTimeout(() => {
+        console.log("Order details fetched");
+        callback();
+    }, 1000);
+}
+
+function makePayment(callback) {
+    setTimeout(() => {
+        console.log("Payment successful");
+        callback();
+    }, 1000);
+}
+
+//Problem ye hai ki jaise-jaise callbacks badhte hain, code read, maintain aur error-handle karna difficult ho jata hai.
+
+//Modern JavaScript me isi problem ko solve karne ke liye pehle Promises aur phir async/await use kiya jata hai.
